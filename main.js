24fbc2df8fd8ca86fb612e5df605a028ca5c6c7c
tackt21 <t-nakagawa@willpride.co.jp>
2022-10-07 16:25:29 +0900

    update

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
-// import { BoxHelper } from "three";
-import bg from "./bg/bg.jpeg"
+import { BoxHelper } from "three";
  
 //canvas
 const canvas = document.querySelector("#webgl");
@@ -12,8 +11,7 @@ const scene = new THREE.Scene();
 
 // 背景用のテクスチャ
 const textureLoader = new THREE.TextureLoader();
-// const bgTexture = textureLoader.load("bg/bg.jpg");
-const bgTexture = textureLoader.load(bg);
+const bgTexture = textureLoader.load("bg/bg.jpg");
 scene.background = bgTexture;
 
 //サイズ
